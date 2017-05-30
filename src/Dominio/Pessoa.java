73bdc03a016db38dc44b1e0b813bf8c69e54708f
reida <reida@172.16.38.78>
2017-05-30 09:46:50 -0300

    Adicionando Dominio Base 3

--- src/Dominio/Pessoa.java
+++ src/Dominio/Pessoa.java
@@ -16,5 +16,67 @@ public abstract class Pessoa {
     private String site;
     private String endereco;
     private Processo[] processos;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, int numero, String email, String site, String endereco, Processo[] processos) {
+        this.nome = nome;
+        this.numero = numero;
+        this.email = email;
+        this.site = site;
+        this.endereco = endereco;
+        this.processos = processos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public Processo[] getProcessos() {
+        return processos;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setProcessos(Processo[] processos) {
+        this.processos = processos;
+    }
+    
+    
     
 }
