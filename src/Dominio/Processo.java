f8e86c0fdb234fb943aff64bbf2f073c86eae7a3
reida <reida@172.16.38.78>
2017-06-06 10:30:59 -0300

    Mapeamento Hibernate de Proceso

--- src/Dominio/Processo.java
+++ src/Dominio/Processo.java
@@ -6,25 +6,60 @@
 package Dominio;
 
 import java.util.ArrayList;
+import javax.persistence.*;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 /**
  *
  * @author reida
  */
+@Entity
+@Table(schema = "ProjectPOO2")
 public class Processo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name="idProcesso")
+    private int idProcesso;
+    @Column(unique = true, nullable = false, insertable = true, updatable = true, length = 45)
     private String Titulo;
+    @Column(unique = true, nullable = false, insertable = true, updatable = true, length = 45)
     private String Pasta;
+    @Column(unique = true, nullable = false, insertable = true, updatable = true)
     private int numero;
+    @Column(unique = true, nullable = false, insertable = true, updatable = true, length = 45)
     private String justiça;
+    @Column(unique = true, nullable = false, insertable = true, updatable = true, length = 45)
     private String instancia;
+    @Column(unique = true, nullable = false, insertable = true, updatable = true, length = 45)
     private String orgao;
+    @Column(unique = true, nullable = false, insertable = true, updatable = true)
     private double valorProcesso;
+    @Column(unique = true, nullable = false, insertable = true, updatable = true, length = 45)
     private String descricao;
+    @Column(unique = true, nullable = false, insertable = true, updatable = true, length = 45)
     private String acao;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name="Responsabiliza", schema = "ProjectPOO2", joinColumns = @JoinColumn(name = "idProcesso"),
+            inverseJoinColumns = @JoinColumn(name = "idAdvogado"))
     private ArrayList<Advogado> advogados;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "Cliente", schema = "ProjectPOO2", joinColumns = @JoinColumn(name = "idProcesso"),
+            inverseJoinColumns = @JoinColumn(name = "idPessoa"))
     private ArrayList<Pessoa> cliente;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idEstadoProcesso", insertable = true, updatable = true, unique = true)
+    @Fetch(FetchMode.JOIN)
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
     private EstadoProcesso estadoProcesso;
+    @OneToMany(mappedBy = "processo", fetch = FetchType.LAZY)
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
     private ArrayList<Audciencia> audiencias;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idPagamento", insertable = true, updatable = true, nullable = false)
+    @Fetch(FetchMode.JOIN)
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
     private Pagamento pagamento;
 
     public Processo() {
